3c4d6551e10ea15b13b0da17f7c8cc793a061d1f
amartyagangwar1 <amartyagangwar1@gmail.com>
2025-08-10 16:36:33 -0500

    updated main to properly implement randomForest model

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from plot import select, plot, choices
+from plot import select, plot, choices, file
+from randomForest import hemIndx, model
 
 def main():
     select()
+    hemIndx()
+    model()
+    ''''
+    choices()
     plot()
-
-
+    '''
 
 if __name__ == "__main__":
     main()    
